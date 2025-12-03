d48cd1b707f1dc6793e13d3a5b166915ca10b52c
aaa <aaaa@com>
2025-12-03 14:28:10 +0500

    Разработанный класс оправки сообщений на почту.Кантуганов

---
+++ RegIN_Kantuganov/Classes/SendMail.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+
+namespace RegIN_Kantuganov.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string message, string to) {
+            var SmtpClient = new SmtpClient()
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(),
+                EnableSsl = true
+            };
+            SmtpClient.Send("landaxer@yandex.ru", to, "Проект RegIN", message);
+        }
+    }
+}
