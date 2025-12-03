5408f46ea8a914d3f1430923577559b6a3692740
aaa <aaaa@com>
2025-12-03 14:24:13 +0500

    Разработанный класс WorkingDB. Кантуганов

---
+++ RegIN_Kantuganov/Classes/WorkingDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+
+namespace RegIN_Kantuganov.Classes
+{
+    public class WorkingDB
+    {
+        readonly static string config = "server=127.0.0.1;uid=root;pwd=;database=regin";
+
+        public static MySqlConnection OpenConnection() {
+
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(config);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex) {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public static MySqlDataReader Query(string SQL, out MySqlConnection connection)
+        {
+            connection = OpenConnection();
+            MySqlCommand command = new MySqlCommand(SQL, connection);
+            return command.ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection connection) {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+        public static bool OpenConnection(MySqlConnection connection) {
+            return connection != null && connection.State == System.Data.ConnectionState.Open;
+        }
+
+    }
+}
